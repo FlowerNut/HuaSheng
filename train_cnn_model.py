a46ccce7ed84f5744aab61e0b8a5c10554c61b22
amwuuuuje <amwuuuuje@163.com>
2023-01-27 12:56:48 +0800

    update

--- train_cnn_model.py
+++ train_cnn_model.py
@@ -3,11 +3,11 @@ from config import label_rules_2days
 
 if __name__ == '__main__':
     #------------- positive model -------------------
-    positive_2day_label_rule = label_rules_2days.Positive2Days()
-    train_data_preparer.TrainingDataPreparer(positive_2day_label_rule, be_continue=True)
-    cnn_model_classifer_2days.Classifier(positive_2day_label_rule.model_file_name)
+    #positive_2day_label_rule = label_rules_2days.Positive2Days()
+    #train_data_preparer.TrainingDataPreparer(positive_2day_label_rule, be_continue=True)
+    #cnn_model_classifer_2days.Classifier(positive_2day_label_rule.model_file_name)
     #------------- negative 2 day model -------------------
-    #negative_2day_label_rule = label_rules_2days.Negative2Days()
-    #train_data_preparer.TrainingDataPreparer(negative_2day_label_rule, be_continue=False)
-    #cnn_model_classifer_2days.Classifier(negative_2day_label_rule.model_file_name)
+    negative_2day_label_rule = label_rules_2days.Negative2Days()
+    train_data_preparer.TrainingDataPreparer(negative_2day_label_rule, be_continue=False)
+    cnn_model_classifer_2days.Classifier(negative_2day_label_rule.model_file_name)
 
