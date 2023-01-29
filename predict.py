9258770f9a0b006d03fb993e04d3cb2b797967ac
amwuuuuje <amwuuuuje@163.com>
2023-01-29 21:57:17 +0800

    update

--- predict.py
+++ predict.py
@@ -7,14 +7,19 @@ if __name__ == '__main__':
     positive_2day_predict_result_file_path = os.path.join(app_config.application_data_directory_path, 'positive_2day_prediction.csv')
     positive_2day_label_rule = label_rules_2days.Positive2Days()
     predict_using_cnn.PredictByCNN(positive_2day_label_rule, positive_2day_predict_result_file_path)
+    # ----------positive predict----------------------
+    #positive_3day_predict_result_file_path = os.path.join(app_config.application_data_directory_path, 'positive_3day_prediction.csv')
+    #positive_3day_label_rule = label_rules_2days.Positive3Days()
+    #predict_using_cnn.PredictByCNN(positive_3day_label_rule, positive_3day_predict_result_file_path)
     # ----------negative predict----------------------
-    #negative_predict_result_file_path = os.path.join(app_config.application_data_directory_path, 'negative_10day_prediction.csv')
-    #negative_10day_label_rule = label_rules_2days.Negative10Days()
-    #predict_using_cnn.PredictByCNN(negative_10day_label_rule, negative_predict_result_file_path)
+    #negative_predict_result_file_path = os.path.join(app_config.application_data_directory_path, 'negative_5day_prediction.csv')
+    #negative_5day_label_rule = label_rules_2days.Negative5Days()
+    #predict_using_cnn.PredictByCNN(negative_5day_label_rule, negative_predict_result_file_path)
     # ----------negative predict----------------------
     negative_2day_predict_result_file_path = os.path.join(app_config.application_data_directory_path, 'negative_2day_prediction.csv')
     negative_2day_label_rule = label_rules_2days.Negative2Days()
     predict_using_cnn.PredictByCNN(negative_2day_label_rule, negative_2day_predict_result_file_path)
     # ---------- combine p and n results --------------
     combine_prediction.Combination(positive_2day_predict_result_file_path, negative_2day_predict_result_file_path, plus_t_minus_f=False)
+    #combine_prediction.Combination(positive_3day_predict_result_file_path, negative_5day_predict_result_file_path, plus_t_minus_f=False)
 
--- train_cnn_model.py
+++ train_cnn_model.py
@@ -14,4 +14,8 @@ if __name__ == '__main__':
     negative_5day_label_rule = label_rules_2days.Negative5Days()
     train_data_preparer.TrainingDataPreparer(negative_5day_label_rule, be_continue=True)
     cnn_model_classifer_2days.Classifier(negative_5day_label_rule.model_file_name)
+    #------------- positive model -------------------
+    #positive_3day_label_rule = label_rules_3days.Positive2Days()
+    #train_data_preparer.TrainingDataPreparer(positive_3day_label_rule, be_continue=False)
+    #cnn_model_classifer_2days.Classifier(positive_3day_label_rule.model_file_name)
 
