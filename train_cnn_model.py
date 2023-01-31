126d3fafe93a7a3ffa5b37e5a06d2ba7d61f06ce
amwuuuuje <amwuuuuje@163.com>
2023-01-31 21:07:20 +0800

    update

--- train_cnn_model.py
+++ train_cnn_model.py
@@ -11,11 +11,11 @@ if __name__ == '__main__':
     #train_data_preparer.TrainingDataPreparer(negative_2day_label_rule, be_continue=True)
     #cnn_model_classifer_2days.Classifier(negative_2day_label_rule.model_file_name)
     #------------- negative 5 day model -------------------
-    negative_5day_label_rule = label_rules_2days.Negative5Days()
-    train_data_preparer.TrainingDataPreparer(negative_5day_label_rule, be_continue=True)
-    cnn_model_classifer_2days.Classifier(negative_5day_label_rule.model_file_name)
+    #negative_5day_label_rule = label_rules_2days.Negative5Days()
+    #train_data_preparer.TrainingDataPreparer(negative_5day_label_rule, be_continue=True)
+    #cnn_model_classifer_2days.Classifier(negative_5day_label_rule.model_file_name)
     #------------- positive model -------------------
-    #positive_3day_label_rule = label_rules_3days.Positive2Days()
-    #train_data_preparer.TrainingDataPreparer(positive_3day_label_rule, be_continue=False)
-    #cnn_model_classifer_2days.Classifier(positive_3day_label_rule.model_file_name)
+    positive_3day_label_rule = label_rules_2days.Positive3Days()
+    train_data_preparer.TrainingDataPreparer(positive_3day_label_rule, be_continue=False)
+    cnn_model_classifer_2days.Classifier(positive_3day_label_rule.model_file_name)
 
